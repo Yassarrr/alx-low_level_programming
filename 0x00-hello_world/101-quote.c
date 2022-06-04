66517faa397a74c9fbd34453fe49b92e583873f5
Abdulyassa Murtala <yassar307@yahoo.com>
2022-06-04 02:12:33 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - Entry Pointr
+*
+* Return: Always 1(Error)
+*/
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar,
+2015-10-19\n", 1, 59, stderr);
+	return (1);
+}
