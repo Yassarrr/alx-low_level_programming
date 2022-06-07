e5fabef3fe23c1890868cb8607de3d173648270e
Abdulyassa Murtala <yassar307@yahoo.com>
2022-06-07 07:47:45 -0700

    1putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- * main - Prints _putchar as a message.
+ * main -Entry point of the program
  *
- * Return: Always 0 (Success)
+ * Description: This program prints _putchar and ends with a new line
+ *
+ * Return: Aways returns(0)
  */
+
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count])
-	}
-	_putchar('\n');
+	_putchar("_putchar('\n')");
 	return (0);
+
 }
