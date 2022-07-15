ff3880c190ac4976df5a963a13bb2a8e92a58314
Abdulyassa Murtala <yassar307@yahoo.com>
2022-07-15 03:21:41 -0700

    0-main.c

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -29,12 +29,7 @@ int main(void)
 	n = print_list(head);
 	printf("-> %lu elements\n", n);
 
-	printf("\n");
-	free(new->str);
-	new->str = NULL;
-	n = print_list(head);
-	printf("-> %lu elements\n", n);
-
+	printf("\n
 	free(new);
 	return (0);
 }
